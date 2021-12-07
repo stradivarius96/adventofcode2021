7129ceebf324a73681ea7a79c9c6aca75aa11ef5
stradivarius96 <matthew.pavan@gmail.com>
2021-12-07 16:25:58 +0000

    Added floor ceil checks day7

--- day7.js
+++ day7.js
@@ -6,15 +6,20 @@ let arr = fs.readFileSync('day7-input.txt').toString().split(",").map(function(i
 
 let arrSort = arr.sort((a,b)=> {return a-b})
 let median = arrSort[arrSort.length/2]
-let mean = Math.floor(arrSort.reduce((pv, cv) => pv + cv, 0)/arrSort.length)
 
 let part1 = arrSort.reduce((pv, cv) => pv + Math.abs(cv - median), 0)
 
 console.log("Part 1 Answer: " + part1)
 
-// Gauss sum formula for fuel betwen two numbers increasing linearly
-let part2 = arrSort.reduce((pv, cv) => pv + (Math.abs(cv - mean) * (Math.abs(cv - mean) + 1) / 2)  , 0)
+// Mean probably isnt an integer, check floor and ceiling results
+let meanCeil = Math.ceil(arrSort.reduce((pv, cv) => pv + cv, 0)/arrSort.length)
+let meanFloor = Math.floor(arrSort.reduce((pv, cv) => pv + cv, 0)/arrSort.length)
 
+// Gauss sum formula for fuel betwen two numbers increasing linearly
+let part2Ceil = arrSort.reduce((pv, cv) => pv + (Math.abs(cv - meanCeil) * (Math.abs(cv - meanCeil) + 1) / 2)  , 0)
+let part2Floor = arrSort.reduce((pv, cv) => pv + (Math.abs(cv - meanFloor) * (Math.abs(cv - meanFloor) + 1) / 2)  , 0)
+
+let part2 = part2Ceil > part2Floor ? part2Floor : part2Ceil
 console.log("Part 2 Answer: " + part2)
 
 console.timeEnd('day7')
