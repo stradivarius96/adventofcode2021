777b2171955a83cbde5764427f9d34bc79d3f9c6
stradivarius96 <matthew.pavan@gmail.com>
2021-12-17 19:30:08 +0000

    day16 Solved

--- day16.js
+++ day16.js
@@ -1,5 +1,4 @@
-console.time('day14')
-const _ = require('lodash')
+console.time('day16')
 const fs = require('fs')
 let packetQueue = []
 
@@ -24,17 +23,16 @@ const hexBinary = {
 }
 
 fs.readFileSync('day16-input.txt').toString().split("").map(hex => packetQueue.push(...hexBinary[hex].split("")))
-const binaryHex = _.reverse(hexBinary)
 
+let versionTotal = 0
 
-// arr.forEach()
 let parentPacket = subPacket({})
 function subPacket() {
 
     let packet = {}
     // every packet has at least 2 hex at beginning for version and type
 
-    console.log(`Packet Queue: ${packetQueue}`)
+    // console.log(`Packet Queue: ${packetQueue}`)
     packet.versionBinary = packetQueue.splice(0, 3).join("")
     packet.version = parseInt(packet.versionBinary, 2)
     packet.typeBinary = packetQueue.splice(0, 3).join("")
@@ -66,9 +64,9 @@ function subPacket() {
         if (packet.lengthTypeId == "0") {
 
             packet.totalLength = parseInt(packetQueue.splice(0, 15).join(""), 2)
-            
+
             let startLength = packetQueue.length
-            while (packetQueue.length >= startLength - packet.totalLength ) {
+            while (packetQueue.length > startLength - packet.totalLength) {
                 packet.subPackets.push(subPacket(packet))
             }
         }
@@ -77,15 +75,62 @@ function subPacket() {
         else if (packet.lengthTypeId == "1") {
             packet.numberofSubPacketsBinary = packetQueue.splice(0, 11).join("")
             packet.numberOfSubPackets = parseInt(packet.numberofSubPacketsBinary, 2)
-    
+
             for (let i = 0; i < packet.numberOfSubPackets; i++) {
                 packet.subPackets.push(subPacket(packet))
             }
         }
 
     }
+    versionTotal += packet.version
+
+    switch (packet.type) {
+        // sum
+        case 0:
+            packet.value = packet.subPackets.reduce((tv, cv) => tv + cv.value, 0)
+            packet.operation = `sum ${packet.subPackets.map(val => val.value).join(" + ")}`
+            break;
+        // product
+        case 1:
+            packet.value = packet.subPackets.reduce((pv, cv) => pv * cv.value, 1)
+            packet.operation = `product ${packet.subPackets.map(val => val.value).join(" * ")}`
+            break;
+        // minimum
+        case 2:
+            packet.value = packet.subPackets.reduce((mv, cv) => cv.value < mv ? cv.value : mv, Infinity)
+            packet.operation = `min ${packet.subPackets.map(val => val.value).join(" , ")}`
+            break;
+        // maximum
+        case 3:
+            packet.value = packet.subPackets.reduce((mv, cv) => cv.value > mv ? cv.value : mv, 0)
+            packet.operation = `max ${packet.subPackets.map(val => val.value).join(" , ")}`
+            break;
+        // literal
+        case 4:
+            packet.value = packet.binaryValue
+            break;
+        // greater than
+        case 5:
+            packet.value = packet.subPackets[0].value > packet.subPackets[1].value ? 1 : 0
+            packet.operation = `greater ${packet.subPackets.map(val => val.value).join(" > ")}`
+            break;
+        // less than
+        case 6:
+            packet.value = packet.subPackets[0].value < packet.subPackets[1].value ? 1 : 0
+            packet.operation = `less ${packet.subPackets.map(val => val.value).join(" < ")}`
+            break;
+        // equal
+        case 7:
+            packet.value = packet.subPackets[0].value == packet.subPackets[1].value ? 1 : 0
+            packet.operation = `equal ${packet.subPackets.map(val => val.value).join(" == ")}`
+            break;
+    }
 
     return packet
 }
 
-console.log(JSON.stringify(parentPacket,null, 2))
+console.log(`Part 1 Answer: ${versionTotal}`)
+
+console.log(`Part 2 Answer: ${parentPacket.value}`)
+
+console.timeEnd('day16')
